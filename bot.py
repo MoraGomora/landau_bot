16c896e5b71a95ab9d5cd63cee60107bb913bdfc
MoraGomora <moragomora30@gmail.com>
2026-04-23 19:42:29 +0300

    Change MongoDB connection failure handling to exit the application

--- bot.py
+++ bot.py
@@ -33,7 +33,7 @@ async def on_startup(owners: list, bot: Bot, container: AppContainer, logger: Fi
                 "Failed to connect to MongoDB. Check internet connection or add new IP address in MongoDB -> Network config"
             )
 
-        return
+        exit(1)
     
     bot_info = await bot.get_me()
     await logger.ainfo(
